6e0b4ef56cfad4421239f072f3c02efb82485c7d
Mike Dragert <mikedragert@gmail.com>
2024-01-22 17:01:10 -0500

    added handling for /hello

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ const PORT = 8080;
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com" 
+  "9sm5xK": "http://www.google.com"
 };
 
 app.get("/", (req, res) => {
@@ -17,4 +17,8 @@ app.listen(PORT, () => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-})
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
